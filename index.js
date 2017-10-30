60ab2d43784f427f841cf8a238f15f5d65286c4e
Mishal Shashikant <mishalmsb@gmail.com>
2017-10-30 23:04:37 +0000

    mongoose setup
    added mongoose, userSchema and tested succesfully adding new record with only googleId

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
-require('./services/passport');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 const authRoutes = require('./routes/authRoutes');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
