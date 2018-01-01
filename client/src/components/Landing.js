ecfcb49b53a494562aca1cb6541d77243dcb5fb7
Mishal Shashikant <mishalmsb@gmail.com>
2018-01-01 21:21:40 +0000

    Added nav logo link

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,13 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Landing extends Component {
-	render() {
-		return (
-			<div>
-				<h2>Landing</h2>
-			</div>
-		);
-	}
-}
+const Landing = () => {
+	return (
+		<div style={{ textAlign: 'center' }}>
+			<h2>Rated!</h2>
+			<p>Collect feedback from your users</p>
+		</div>
+	);
+};
 
 export default Landing;
+
+// import React, { Component } from 'react';
+//
+// class Landing extends Component {
+// 	render() {
+// 		return (
+// 			<div>
+// 				<h2>Landing</h2>
+// 			</div>
+// 		);
+// 	}
+// }
+//
+// export default Landing;
