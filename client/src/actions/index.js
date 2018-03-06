ba51b31ac685987fc6001a063cba0296766f717f
Mishal Shashikant <mishalmsb@gmail.com>
2018-03-06 21:50:51 +0000

    displaying list

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { FETCH_USER } from './types';
+import { FETCH_USER, FETCH_SURVEYS } from './types';
 
 export const fetchUser = () => async dispatch => {
 	const res = await axios.get('/api/current-user');
@@ -20,10 +20,9 @@ export const submitSurvey = (values, history) => async dispatch => {
 	dispatch({ type: FETCH_USER, payload: res.data });
 };
 
-// export const fetchUser = () => {
-// 	return function(dispatch) {
-// 		axios
-// 			.get('/api/current-user')
-// 			.then(res => dispatch({ type: FETCH_USER, payload: res }));
-// 	};
-// };
+export const fetchSurveys = () => async dispatch => {
+	const res = await axios.get('/api/surveys');
+
+	dispatch({ type: FETCH_SURVEYS, payload: res.data });
+};
+
